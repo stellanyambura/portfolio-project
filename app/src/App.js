fb1ae0d4486fb0dee5e250dcc733e00adc0e6698
Stellanyambura <stellagitonga.com>
2023-03-02 14:33:42 +0300

    added home

--- app/src/App.js
+++ app/src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import './App.css';
-import Login from './Login';
+import Login from './components/Login';
 
 const App = () => {
   return (
---
+++ app/src/components/Home.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+function Home() {
+  return (
+    <div>
+      <h1>Welcome to My Portfolio</h1>
+      <p>
+        
+      </p>
+    </div>
+  );
+}
+
+export default Home;
