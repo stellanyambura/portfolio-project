2ae132465802b2a66e7ffcf14e2fe0be8084522b
Stellanyambura <stellagitonga.com>
2023-03-03 09:50:04 +0300

    added files

--- app/src/components/Navbar.js
+++ app/src/components/Navbar.js
@@ -7,17 +7,16 @@ const Nav=()=>{
   return(
     <>
   <nav className="navbar bg-dark" style={{height: "150%", fontSize: "1.5rem", color:'white'}}>
-      <NavLink to="/">Home</NavLink>
-      <NavLink to="/login">Login</NavLink>
-      </nav>
-      <div className ="home" ></div>
+      <NavLink to="/" style={{backgroundColor:'blue'}}>Home</NavLink>
+     
+      {/* <NavLink to="/randommemes" style={{backgroundColor:'transparent' ,hover:'red'}}>All memes</NavLink> */}
+      <NavLink to="/login" style={{paddingRight: "15px",}}>Login</NavLink>
+     
       
 
-<div className="your-component-class"> {/* Your component content */} </div> 
+    
+</nav>
 </>
   )
-
-
-
 }
 export default Nav
