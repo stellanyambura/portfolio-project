784b2acbf93bb5038166334df51b3252f2681b72
Stellanyambura <stellagitonga.com>
2023-03-03 10:11:02 +0300

    added Users.js

--- app/src/components/Users.js
+++ app/src/components/Users.js
@@ -1,1 +1,25 @@
-import React from 'react';
+function Users() {
+    const [users, setUsers] = useState([]);
+  
+    useEffect(() => {
+      axios.get('/api/users')
+        .then(response => setUsers(response.data))
+        .catch(error => console.log(error));
+    }, []);
+  
+    return (
+      <div>
+        <h1>Users</h1>
+        <ul>
+          {users.map(user => (
+            <li key={user.id}>
+              {user.first_name} {user.last_name}
+            </li>
+          ))}
+        </ul>
+      </div>
+    );
+  }
+  
+  export default Users;
+  
