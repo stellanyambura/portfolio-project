3ea280ec5e566759d6c2d8a134b20d8e3efc01ee
Stellanyambura <stellagitonga.com>
2023-03-03 10:09:49 +0300

    added Project.js

--- app/src/App.js
+++ app/src/App.js
@@ -5,6 +5,7 @@ import Home from './components/Home';
  import './App.css';
 import Login from './components/Login';
 import 'semantic-ui-css/semantic.min.css'
+import ProjectList from './components/Projects';
 
 
 
@@ -20,6 +21,7 @@ function App() {
               <Routes>
                   <Route path='/login' element={<Login/>}/>
                   <Route path='/' element={<Home/>}/>
+                  <Route path='/project' element={<Project/>}/>
                  </Routes>
           </BrowserRouter>
       </div>
